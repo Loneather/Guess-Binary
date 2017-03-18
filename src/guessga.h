3c3883611c604d2a2c33d379d8b978a9c6dab261
Giorgos <geo.giatrakos@gmail.com>
2017-03-18 23:51:35 +0200

    Added guards at header file

--- src/guessga.h
+++ src/guessga.h
@@ -1,5 +1,10 @@
+#ifndef __GUESS__
+#define __GUESS__
+
 int random0or1(void) ;
 double random0to1(void) ;
 int fitness(int * ,int * ,int) ;
 int rand_interval(int ) ;
 void crossover(int * ,int * ,int ) ;
+
+#endif
